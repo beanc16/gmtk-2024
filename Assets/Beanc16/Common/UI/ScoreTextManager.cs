ae4482678c1abf388bd483af8f021f54725620b2
beanc16 <bean.christopher@outlook.com>
2024-08-19 12:59:57 -0400

    Create score manager scripts

---
+++ Assets/Beanc16/Common/UI/ScoreTextManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using TMPro;
+
+namespace Beanc16.Common.UI
+{
+    public class ScoreTextManager : TextManager
+    {
+        [SerializeField]
+        private string scorePrefix = "Score: ";
+
+        [SerializeField]
+        private int score = 0;
+
+        public void SetText(string text)
+        {
+            SetText(scorePrefix + text);
+        }
+
+        public void SetText(int number)
+        {
+            SetText(scorePrefix + number.ToString());
+        }
+
+        public void SetScore(int number)
+        {
+            score = number;
+            SetText(score);
+        }
+
+        public void IncrementScore(int number)
+        {
+            score += number;
+            SetText(score);
+        }
+    }
+}
